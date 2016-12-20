d070e050c1c87a10267be7706ca622565e09f991
Gabriele Santomaggio <gabriele.santomaggio@erlang-solutions.com>
2016-12-20 17:44:37 +0100

    handler

---
+++ slides/src/eventsource_handler.erl
@@ -0,0 +1,20 @@
+%% @doc EventSource emitter.
+-module(eventsource_handler).
+
+-export([init/2]).
+-export([info/3]).
+
+init(Req0, Opts) ->
+    Req = cowboy_req:stream_reply(200, #{
+        <<"content-type">> => <<"text/event-stream">>
+    }, Req0),
+    erlang:send_after(1000, self(), {message, "Tick"}),
+    {cowboy_loop, Req, Opts, 5000}.
+
+info({message, Msg}, Req, State) ->
+    cowboy_req:stream_body(["id: ", id(), "\ndata: ", Msg, "\n\n"], nofin, Req),
+    erlang:send_after(1000, self(), {message, "Tick"}),
+    {ok, Req, State}.
+
+id() ->
+    integer_to_list(erlang:unique_integer([positive, monotonic]), 16).
