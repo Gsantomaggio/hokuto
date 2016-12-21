618333ac434f403135c3e60c93c5f733e569a1f9
Gabriele Santomaggio <gabriele.santomaggio@erlang-solutions.com>
2016-12-21 23:47:17 +0100

    fix node name

--- slides/run.sh
+++ slides/run.sh
@@ -6,7 +6,7 @@ export RELX_REPLACE_OS_VARS=true
 for i in `seq 1 1`;
 do
     echo "run..$i"
-    NODE_NAME=node_$i  PORT=808$i _build/default/rel/slides/bin/slides-0.0.1 console
+    NODE_NAME=node_$i  PORT=808$i _build/default/rel/slides/bin/slides console
     sleep 1
 done  
 
