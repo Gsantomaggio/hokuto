195d9799121e5a8aa071ab3e6c27310f5c6828c7
Gabriele Santomaggio <g.santomaggio@gmail.com>
2018-03-15 08:56:14 +0100

    Add reconn

--- db_beam_elixir/db_beam/lib/rabbitmq_int.ex
+++ db_beam_elixir/db_beam/lib/rabbitmq_int.ex
@@ -1,14 +1,50 @@
 defmodule RabbitmqInt do
   @moduledoc false
-  # import "amqp_client.hrl"
 
+  # RabbitmqSup.Supervisor.start_link
+  #:sys.trace Process.whereis(:rabbitmq_gen), true
+  #:dbg.start(); :dbg.tracer()
+  #:dbg.tracer()
+  #:dbg.tpl RabbitmqInt, :_ , []
+  #:dbg.tpl RabbitmqInt,:publish_message, :_ , []
+  #:dbg.tpl RabbitmqInt,:publish_message, :_ , [{:_, [], [{:return_trace}]}]
+  #:dbg.tpl RabbitmqInt,:publish_message, 1 , [{:_, [], [{:return_trace}]}]
+  #:dbg.p(:all, :c) or :dbg.p(:all, [:c,:timestamp])
+  #:dbg.:dbg.stop_clear()
+
+
+  # TRACE TO FILE
+  #:dbg.start()
+  #:dbg.tracer(:port,:dbg.trace_port(:file,'/tmp/rmqtrace_test2'))
+  #:dbg.tpl RabbitmqInt, :_ , []
+  #:dbg.p(:all, :c)
+  #erl -eval 'dbg:trace_client(file, "/tmp/rmqtrace_test2").' > rmq
+
+  #using recon
+  #:recon_trace.calls({RabbitmqInt, :_, []}, 100)
+  #:recon_trace.calls({RabbitmqInt, :_, [{:_, [], [{:return_trace}]}]}, 100)
+  #:recon_trace.calls({RabbitmqInt, :publish_message, []}, 100)
+  #:recon_trace.calls({RabbitmqInt, :publish_message, [{:_, [], [{:return_trace}]}]}, 100)
+  #:recon_trace.clear()
+
+
+  #Debug
+  #:debugger.start()
+  #:int.ni(RabbitmqInt)
+  #:int.break(RabbitmqInt, 103)
+
+
+  ## Note about the sender PID
   require Record
   @behaviour :gen_statem
   @name :rabbitmq_gen
 
+  def publish_message(prefix, message) do
+    :gen_statem.call(@name, {:publish, Enum.join([prefix, message], "_")})
+  end
+
   def publish_message(message) do
     :gen_statem.call(@name, {:publish, message})
-
   end
 
   def start_link() do
@@ -39,18 +75,18 @@ defmodule RabbitmqInt do
 
     case AMQP.Connection.open do
       {:ok, %AMQP.Connection{pid: pid}} -> Blogs.log_info("Connection established, PID: #{inspect(pid)}")
-                     Process.link(pid)
-                     {
-                       :next_state,
-                       :publish,
-                       state,
-                       {
-                         :next_event,
-                         {:call, from},
-                         {:publish, message}
-                       }
+                                           Process.link(pid)
+                                           {
+                                             :next_state,
+                                             :publish,
+                                             state,
+                                             {
+                                               :next_event,
+                                               {:call, from},
+                                               {:publish, message}
+                                             }
 
-                     }
+                                           }
 
 
       error -> Blogs.log_info("Connection error, Reason: #{inspect(error)}")
--- db_beam_elixir/db_beam/mix.exs
+++ db_beam_elixir/db_beam/mix.exs
@@ -21,7 +21,8 @@ defmodule DB_BEAM.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:amqp, "~> 0.2.3"}
+      {:amqp, "~> 0.2.3"},
+      {:recon, "~> 2.3"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
